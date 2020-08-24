6a21ce8b28061192fadb84a4eed27958c73739d2
Shaurya Bhandari <shauryabhandari25@gmail.com>
2020-08-24 22:40:25 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">hi</div>;
+  return <div className="App">this is a form in react</div>;
 }
 
 export default App;
