72d65aa1d8b39bfc769ad6723b98c36f47ee761e
Shaurya Bhandari <shauryabhandari25@gmail.com>
2020-08-26 13:13:53 +0530

    file structure made

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">this is a form in react</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
